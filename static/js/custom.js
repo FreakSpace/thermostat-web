cb305ba5cc5fb4559e306d2007f9e0642282e7d7
FuckingHacking <navchalkin@ex.ua>
2019-04-24 01:14:20 +0300

    created page with all records. added pagination, ajax-request for getting single record.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,10 @@
-
 function formatDate(date) {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
-        hours = d.getHours();
-        minutes = d.getMinutes();
+    hours = d.getHours();
+    minutes = d.getMinutes();
 
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
@@ -15,12 +14,85 @@ function formatDate(date) {
     return [year, month, day, hours, minutes].join('-');
 }
 
-$( "#get-data-button" ).on( "click", function() {
+$("#get-data-button").on("click", function () {
     var from_date = formatDate($("#datetimepicker_from").datetimepicker('viewDate')._d);
     var to_date = formatDate($("#datetimepicker_to").datetimepicker('viewDate')._d);
-    window.location.replace("/?start_date="+from_date+"&end_date="+to_date);
+    window.location.replace("/?start_date=" + from_date + "&end_date=" + to_date);
 });
 
-$( "#clean-data-button" ).on( "click", function() {
+$("#clean-data-button").on("click", function () {
     window.location.replace("/");
+});
+
+
+$("tr").on("click", function () {
+    var _id = $(this).data("value");
+    $.ajax({
+        url: "single/?id=" + _id,
+        data: _id,
+        dataType: 'json',
+        success: function (data) {
+            $("#detail-data").show("slow");
+
+            $("#th-id").text(data['id']);
+            $("#th-date").text(data['date']);
+            $("#th-state").text(function () {
+                if (data['thermostat_state']) {
+                    $(this).addClass("badge-success");
+                    $(this).removeClass("badge-secondary");
+                    return "Увімкнено";
+                }
+                else {
+                     $(this).removeClass("badge-success");
+                    $(this).addClass("badge-secondary");
+                    return "Вимкнено"
+                }
+            });
+            $("#th-temp").text(data['temp'] + "°C");
+            $("#th-set-t").text(data['set_temp'] + "°C");
+            $("#th-co2").text(data['co2'] + "");
+            $("#th-set-co2").text(data['set_co2'] + "");
+
+            $("#th-light").text(function () {
+                if (data['light']) {
+                    $(this).removeClass("badge-secondary");
+                    $(this).addClass("badge-warning");
+                    return "Увімкнено";
+                }
+                else {
+                    $(this).removeClass("badge-warning");
+                    $(this).addClass("badge-secondary");
+                    return "Вимкнено"
+                }
+            });
+
+            $("#th-color-light").text(function () {
+                $(this).css("background-color", 'rgb('+data['light_R']+','+data['light_G']+','+data['light_B']+')')
+            });
+
+            $("#th-work").text(function () {
+                data = data['current_state'];
+                if (data === "1") {
+                    $(this).removeClass("badge-primary");
+                    $(this).removeClass("badge-secondary");
+                    $(this).addClass("badge-danger");
+                    $(this).text("Нагрівання");
+                }
+
+                else if (data === "2") {
+                    $(this).removeClass("badge-danger");
+                    $(this).removeClass("badge-secondary");
+                    $(this).addClass("badge-primary");
+                    $(this).text("Охолодження");
+                }
+
+                else {
+                    $(this).removeClass("badge-danger");
+                    $(this).removeClass("badge-primary");
+                    $(this).addClass("badge-secondary");
+                    $(this).text("Не працює");
+                }
+            });
+        }
+    })
 });
--- ts/views.py
+++ ts/views.py
@@ -1,6 +1,35 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import LogThermostat
 from datetime import datetime, timedelta
+
+
+from django.http import JsonResponse
+
+def get_single(request):
+    data = {}
+    id_elem = request.GET.get('id')
+    record = LogThermostat.objects.get(id=id_elem)
+    data["date"] = record.time.strftime('%H:%M | %d-%m-%Y')
+    data["id"] = record.id
+    data["thermostat_state"] = record.thermostat_state
+    data["current_state"] = record.current_state
+    data["temp"] = record.temp
+    data["set_temp"] = record.set_temp
+    data["co2"] = record.co2
+    data["set_co2"] = record.set_co2
+    data["light"] = record.light
+    data["light_R"] = record.light_R
+    data["light_G"] = record.light_G
+    data["light_B"] = record.light_B
+    return JsonResponse(data)
+
+
+class AllDataView(ListView):
+    model = LogThermostat
+    template_name = "ts/all_data.html"
+    context_object_name = 'records'
+    paginate_by = 50
+    queryset = LogThermostat.objects.all().order_by('-time')
 
 
 class DataListView(ListView):
